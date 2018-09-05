61e52054e0d7959ddf1b4e21e42ef90f8e4ce7c1
Milad Vafaeifard <milad_vafaeifard@epam.com>
2018-09-05 17:46:05 +0200

    Updates

---
+++ precache-manifest.4e12d0eecf574a0e00182d470ee074e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3bb3133ab71f71e43095",
+    "url": "/js/chunk-vendors.88e8071c.js"
+  },
+  {
+    "revision": "81e2fb0ef4f6eb3201bd",
+    "url": "/js/app.188f2432.js"
+  },
+  {
+    "revision": "6aeca4fef8584fc66fe0281fdd209126",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bb3133ab71f71e43095",
+    "url": "/css/chunk-vendors.38fecd20.css"
+  },
+  {
+    "revision": "81e2fb0ef4f6eb3201bd",
+    "url": "/css/app.e40a5254.css"
+  }
+];
